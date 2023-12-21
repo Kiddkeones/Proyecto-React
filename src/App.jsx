9cae0517c409a2a29f59bc132e5c2258c7e9a60f
alejod <dedominicci.alejo@gmail.com>
2023-12-20 21:02:55 -0300

    Agregue productos y me falta agregar la parte de router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-//Componentes
+import { useEffect, useState } from 'react';
 import { NavBarComponent } from './components/NavBarComponent/NavBarComponent';
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
-
-//Styles
+import { getProducts } from './services';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [productsData, setProductsData] = useState ([])
+  
+  
+  useEffect(() =>{
+    getProducts()
+    .then(Response =>{
+      setProductsData(Response.data.products)
+    })
+  }, [])
+
   return (
     <div style={{width: "100vw", height: "100vh"}}>
       <NavBarComponent />
-      <ItemListContainer greeting="Bienvenidos a mi tienda" />
+      <ItemListContainer productsData={productsData} />
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,27 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import "./item.css"
 
-export const ItemListContainer = ({greeting}) => {
+export const ItemListContainer = ({ productsData }) => {
   return (
-    <div className="container">{greeting}</div>
+    <div className='cards'>
+      {
+        productsData.map(products => {
+          return (
+            <Card key={products.id} style={{ width: '18rem'}}>
+              <Card.Img variant="top" src={products.thumbnail}/>
+              <Card.Body>
+                <Card.Title>{products.title}</Card.Title>
+                <Card.Text>
+                  {products.description}
+                </Card.Text>
+                <div>{products.price}</div>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          );
+      })
+      }
+    </div>
   )
 }
